70e03e8e45f858080ba1be6c2e998df2b3cd42d1
sara <sarah.ibrahim1855@gmail.com>
2020-02-14 22:43:29 +0200

    update

---
+++ lib/pages/result_page.dart
@@ -0,0 +1,73 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/utilities/bottom_button.dart';
+import 'package:bmi_calculator/utilities/reusable_card.dart';
+import 'package:flutter/material.dart';
+
+
+
+class ResultPage extends StatelessWidget {
+  ResultPage({this.bmi, this.interpretation, this.result});
+  final String bmi;
+  final String result;
+  final String interpretation;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI '),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              'Yor Result',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    result,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 30,
+                    ),
+                  ),
+                  Text(
+                    bmi,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    interpretation,
+                    textAlign: TextAlign.center,
+                    style: kLabelTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            text: 'RE-CALCULATE',
+            onPress: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
